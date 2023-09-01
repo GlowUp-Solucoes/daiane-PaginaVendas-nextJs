7f92f2573125411965e53a22ffb5e0bec69cbd21
Daniel Heringer <danielheringers@icloud.com>
2023-08-31 21:43:38 -0300

    Corrigindo Pixel do Facebook

--- src/components/FacebookPixel/FacebookPixel.tsx
+++ src/components/FacebookPixel/FacebookPixel.tsx
@@ -1,28 +1,45 @@
 'use client'
-
-import { usePathname } from 'next/navigation'
 import Script from 'next/script'
 import { useEffect, useState } from 'react'
 import * as pixel from '../../../lib/fpixel'
+import { useRouter } from 'next/router'
 
 const FacebookPixel = () => {
-  const [loaded, setLoaded] = useState(false)
-  const pathname = usePathname()
+  const router = useRouter()
 
   useEffect(() => {
-    if (!loaded) return
-
     pixel.pageview()
-  }, [pathname, loaded])
+
+    const handleRouteChange = () => {
+      pixel.pageview()
+    }
+    
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
       <Script
         id="fb-pixel"
-        src="../../../lib/fpixel"
         strategy="afterInteractive"
-        onLoad={() => setLoaded(true)}
-        data-pixel-id={pixel.FB_PIXEL_ID}
+        dangerouslySetInnerHTML={{
+          __html:`
+          <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${pixel.FB_PIXEL_ID}); 
+          </script>
+          `,
+        }}
       />
     </>
   )
